554b53d4d1f4fb1afebb078fcd13b95b11dec8a1
Alex Surtees <ats21994@gmail.comn>
2026-03-10 10:40:02 +0000

    Fix CI tests

--- viewer/tests/ozx.test.js
+++ viewer/tests/ozx.test.js
@@ -4,8 +4,8 @@ import { createSourceData } from "../src/io";
 test("Can read .ozx file without error", async () => {
   const baseUrl = `http://${process.env.VITE_TEST_STATIC_SERVER_HOST}:${process.env.VITE_TEST_STATIC_SERVER_PORT}`;
   const image = "backpack.ozx";
-
   const url = `${baseUrl}/${image}`;
+
   const config = {
     source: url,
   };
