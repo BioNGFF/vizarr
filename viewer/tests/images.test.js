a34cd2b97beb35a74994841bb6587d433ea6037c
Alex Surtees <ats21994@gmail.comn>
2026-03-17 11:18:57 +0000

    Lint

--- viewer/tests/images.test.js
+++ viewer/tests/images.test.js
@@ -1,23 +1,21 @@
+import fs from "node:fs";
+import path from "node:path";
 import { test } from "vitest";
+import yaml from "yaml";
 import { createSourceData } from "../src/io";
-import { getYamlFileNames, writeImageYaml } from './metadata'
-import fs from 'fs'
-import path from 'path'
-import yaml from 'yaml'
+import { getYamlFileNames, writeImageYaml } from "./metadata";
 
-const imagesPath = path.resolve(path.join(__dirname, '..', '..', 'fixtures'))
+const imagesPath = path.resolve(path.join(__dirname, "..", "..", "fixtures"));
 
-const files = getYamlFileNames(imagesPath)
+const files = getYamlFileNames(imagesPath);
 
 files.map(async (file) => {
-  const filePath = path.join(imagesPath, file)
-  const description = yaml.parse(fs.readFileSync(filePath, 'utf8'))
+  const filePath = path.join(imagesPath, file);
+  const description = yaml.parse(fs.readFileSync(filePath, "utf8"));
   test(`Can read ${description.name} without error`, async () => {
-    await createSourceData(
-      {
-        source: description.source
-      }
-    )
-    await writeImageYaml(description.source, description.name, imagesPath)
-  })
-})
+    await createSourceData({
+      source: description.source,
+    });
+    await writeImageYaml(description.source, description.name, imagesPath);
+  });
+});
--- viewer/tests/metadata.js
+++ viewer/tests/metadata.js
@@ -1,23 +1,23 @@
-import * as utils from '../src/utils'
-import fs from 'fs'
-import path from 'path'
-import { stringify } from 'yaml';
+import fs from "node:fs";
+import path from "node:path";
+import { stringify } from "yaml";
+import * as utils from "../src/utils";
 
 export async function writeImageYaml(url, imageName, savePath) {
-  const node = await utils.open(url)
-  const attrs = utils.resolveAttrs(node.attrs)
+  const node = await utils.open(url);
+  const attrs = utils.resolveAttrs(node.attrs);
   const metadata = {
     source: url,
     name: imageName,
     type: attrs.multiscales[0].type,
     version: attrs.version,
     features: {
-      multiscale: utils.isMultiscales(attrs)
-    }
-  }
-  fs.writeFileSync(path.join(savePath, `${imageName}.yaml`), stringify(metadata))
+      multiscale: utils.isMultiscales(attrs),
+    },
+  };
+  fs.writeFileSync(path.join(savePath, `${imageName}.yaml`), stringify(metadata));
 }
 
 export function getYamlFileNames(filepath) {
-  return fs.readdirSync(filepath).filter((fileName) => fileName.endsWith('.yaml'))
+  return fs.readdirSync(filepath).filter((fileName) => fileName.endsWith(".yaml"));
 }
