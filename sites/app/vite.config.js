0f539c1e23b3bcc4da3912adb8555b4be67bd22e
Alex Surtees <ats21994@gmail.comn>
2026-03-27 12:30:19 +0000

    Linting

--- sites/app/vite.config.js
+++ sites/app/vite.config.js
@@ -5,13 +5,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: './',
+  base: "./",
   resolve: {
     alias: {
       ...(mode === "development"
         ? {
-          "@biongff/vizarr": path.resolve(__dirname, "../../viewer/src/index.tsx"),
-        }
+            "@biongff/vizarr": path.resolve(__dirname, "../../viewer/src/index.tsx"),
+          }
         : {}),
     },
   },
