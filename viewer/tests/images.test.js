c20db5b50c17f86ff14b322f947bff98d332b65c
Alex Surtees <ats21994@gmail.comn>
2026-05-18 09:36:19 +0100

    Re-enable tests

--- viewer/tests/images.test.js
+++ viewer/tests/images.test.js
@@ -13,9 +13,9 @@ files.map(async (file) => {
   const filePath = path.join(imagesPath, file);
   const description = yaml.parse(fs.readFileSync(filePath, "utf8"));
   test(`Can read ${description.name} without error`, async () => {
-    // await createSourceData({
-    //   source: description.source,
-    // });
-    // await writeImageYaml(description.source, description.name, imagesPath);
+    await createSourceData({
+      source: description.source,
+    });
+    await writeImageYaml(description.source, description.name, imagesPath);
   }, 20000);
 });
