97338fc7903b1910f68549f78b2526902e74216f
Alex Surtees <ats21994@gmail.comn>
2026-05-18 11:30:26 +0100

    Ignore tests in code coverage

--- viewer/tests/images.test.js
+++ viewer/tests/images.test.js
@@ -12,12 +12,13 @@ const files = getYamlFileNames(imagesPath);
 files.map(async (file) => {
   const filePath = path.join(imagesPath, file);
   const description = yaml.parse(fs.readFileSync(filePath, "utf8"));
-  test(`Can read ${description.name} without error`, async () => {
-    if (description.testable) {
+
+  if (description.testable) {
+    test(`Can read ${description.name} without error`, async () => {
       await createSourceData({
         source: description.source,
       });
       await writeImageYaml(description.source, description.name, imagesPath);
-    }
-  }, 20000);
+    }, 20000);
+  }
 });
